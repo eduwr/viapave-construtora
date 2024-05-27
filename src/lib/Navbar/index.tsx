3095dee730753e3f7ec2ad92b6783e3469af413d
Eduardo Ricardo <eduwric@gmail.com>
2024-05-27 12:52:31 -0300

    hanburger menu

--- src/lib/Navbar/index.tsx
+++ src/lib/Navbar/index.tsx
@@ -1,5 +1,7 @@
+'use client'
 import { Logo } from "../Logo";
 import { HamburgerMenuIcon } from "@radix-ui/react-icons";
+import { useState } from "react";
 const links = [
   {
     text: "Início",
@@ -17,12 +19,22 @@ const links = [
 ];
 
 export const Navbar = () => {
- 
+  const [menuOpen, setMenuOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
+
+  const iconSize = {
+    width: 24,
+    height: 24
+  }
+
   return (
     <nav className="flex items-center justify-between px-4">
       <Logo bgDark />
-      <button type="button" className="md:hidden">
-        <HamburgerMenuIcon color="white"/>
+      <button onClick={toggleMenu} type="button" className="md:hidden">
+        <HamburgerMenuIcon {...iconSize} color="white" />
       </button>
       <div className="hidden md:flex justify-end flex-1 items-center">
         {links.map((l) => (
@@ -35,6 +47,19 @@ export const Navbar = () => {
           </a>
         ))}
       </div>
+      {menuOpen && (
+        <div className="fixed right-0 top-16 w-28 bg-gray-800 md:hidden flex flex-col items-center">
+          {links.map((l) => (
+            <a
+              key={l.text}
+              className="p-5 text-white w-full text-center"
+              href={l.link}
+            >
+              {l.text}
+            </a>
+          ))}
+        </div>
+      )}
     </nav>
   );
 };
